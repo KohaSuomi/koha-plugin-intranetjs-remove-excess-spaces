0f9d7c99497d23dd9589a7006708756e8e8f4745
lmstrand <lmstrand@gmail.com>
2024-08-19 13:14:31 +0300

    Add script

--- Koha/Plugin/Fi/KohaSuomi/RemoveExcessSpaces.pm
+++ Koha/Plugin/Fi/KohaSuomi/RemoveExcessSpaces.pm
@@ -22,7 +22,7 @@ our $metadata = {
     minimum_version => '23.11',
     maximum_version => '',
     version         => $VERSION,
-    description     => "Positaa ylimääräisiä välilyöntejä",
+    description     => "Poistaa ylimääräisiä välilyöntejä asiakkaan muokkausnäytön kentistä, niteen muokkausnäytön kentistä, kausijulkaisun vastaanottonäytön kentistä ja uuden tilauksen niteen muokkausnäytön kentistä",
 };
 
 ## This is the minimum code required for a plugin's 'new' method
--- Koha/Plugin/Fi/KohaSuomi/RemoveExcessSpaces/script.js
+++ Koha/Plugin/Fi/KohaSuomi/RemoveExcessSpaces/script.js
@@ -1,1 +1,74 @@
-
+/// ALKU ///
+  
+  /// Ylimääräisten välilyöntien poistot ///
+  
+  /* Poista asiakkaan muokkausnäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä*/
+  $(document).ready(function() {
+    $('body#pat_memberentrygen.pat input').blur(function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');     
+       $(this).val(tmp);
+    });
+  });
+  
+  /* Poista asiakkaan muokkausnäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä*/
+  $(document).ready(function() {
+    $('body#pat_memberentrygen.pat textarea').blur(function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');     
+       $(this).val(tmp);
+    });
+  });
+  
+  /* Poista niteen muokkausnäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä*/
+  $(document).ready(function() {
+    $('body#cat_additem.cat input').blur(function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');
+       $(this).val(tmp);
+    });
+  });
+  
+  /* Poista kausijulkaisun vastaanottonäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä*/
+  $(document).ready(function() {
+    $('body#ser_serials-edit.ser input').blur(function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');
+       tmp = tmp.replace(/(^[1-2][0-9]{3}) *: */, '$1 : ');
+       $(this).val(tmp);
+    });
+  });
+  
+  /* Poista hankinnassa uuden tilauksen niteen muokkausnäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä */
+  /* Tämä ei toimi */
+  $(document).ready(function() {
+    $('body#acq_neworderempty.acq').on("blur", "input", function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');
+       $(this).val(tmp);
+    });
+  });
+  
+  /* Poista hankinnassa uuden tilauksen niteen muokkausnäytön kentistä välilyönnit alusta, lopusta ja useammat peräkkäiset välilyönnit välistä */ 
+  /* Tämä ei toimi */
+  $(document).ready(function() {
+    $('body#acq_neworderempty.acq').on("blur", "textarea", function() {
+       var tmp = $(this).val();
+       tmp = tmp.replace(/^ +/, '');
+       tmp = tmp.replace(/ +$/, '');
+       tmp = tmp.replace(/  */g, ' ');
+       $(this).val(tmp);
+    });
+  });
+  
+  /// LOPPU ///
